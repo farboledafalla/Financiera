4321a126a1abd7f99ea3878dfeebff029e174a98
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2023-11-17 21:18:48 -0500

    Agregar Sidebar

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-/* Importar imagenes */
-import Logo from 'media/logo.png'
+import ImagenLogo from 'components/ImagenLogo';
 
 const AuthLayout = () => {
-  return (
-    <div className='bg-gray-50 flex flex-col items-center justify-center py-2 px-4'>
-      <div className='w-full flex items-start'>
-        <Link to='/'>
-          <i className='fas fa-home cursor-pointer hover:text-indigo-500' />
-        </Link>
+   return (
+      <div className='bg-gray-50 flex flex-col items-center justify-center py-2 px-4'>
+         <div className='w-full flex items-start'>
+            <Link to='/'>
+               <i className='fas fa-home cursor-pointer hover:text-indigo-500' />
+            </Link>
+         </div>
+         <div className='w-full max-w-md'>
+            <ImagenLogo />
+            <Outlet />
+         </div>
       </div>
-      <div className='w-full max-w-md'>
-        <img className='mx-auto h-40 w-auto' src={Logo} alt='Logo' />
-        <Outlet />        
-      </div>
-    </div>
-  )
-}
+   );
+};
 
-export default AuthLayout
+export default AuthLayout;
